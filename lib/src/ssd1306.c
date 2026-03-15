503a212adf8916d0cae3c261147ef212ac457da2
zNitche <53448802+zNitche@users.noreply.github.com>
2026-03-15 17:29:42 +0000

    refactor: defines + i2c related changes

--- lib/src/ssd1306.c
+++ lib/src/ssd1306.c
@@ -1,14 +1,16 @@
 #include "pico_ssd1306/ssd1306.h"
+#include "pico_ssd1306/communication.h"
+#include "pico_ssd1306/defines.h"
 
 #include "hardware/i2c.h"
 #include "pico/stdlib.h"
 
 void ssd1306_setup_i2c(SSD1306_I2C i2c_c) {
-    i2c_init(i2c_c.i2c, 100000);
+    i2c_init(i2c_c.i2c, 400000);
 
     gpio_set_function(i2c_c.sda_pin, GPIO_FUNC_I2C);
-    gpio_pull_up(i2c_c.sda_pin);
-
     gpio_set_function(i2c_c.scl_pin, GPIO_FUNC_I2C);
+
+    gpio_pull_up(i2c_c.sda_pin);
     gpio_pull_up(i2c_c.scl_pin);
 }
