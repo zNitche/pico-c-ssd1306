abc5253883ea753a3a43845a9bafc1fa884d294d
zNitche <53448802+zNitche@users.noreply.github.com>
2026-03-22 19:12:19 +0000

    refactor: tools - convert_to_bitmap.py - modularization

--- tools/convert_to_bitmap.py
+++ tools/convert_to_bitmap.py
@@ -8,6 +8,54 @@ MAX_IMG_HEIGHT = 64
 BITS_PER_BYTE = 8
 
 
+def __create_columns(pixels: list[int],
+                     img_width: int, img_height: int) -> list[list[int]]:
+
+    bits_columns_segments: list[list[int]] = []
+
+    for columns_row_id in range(img_height // BITS_PER_BYTE):
+        column_start_offset = columns_row_id * BITS_PER_BYTE
+
+        for column_index in range(img_width):
+            column_bits: list[int] = []
+
+            column_start_bit_index = column_start_offset + \
+                (column_index * img_width)
+
+            for bit_index in range(BITS_PER_BYTE):
+                target_bit_index = column_start_bit_index + bit_index
+                column_bits.append(pixels[target_bit_index])
+
+            bits_columns_segments.append(column_bits)
+
+    return bits_columns_segments
+
+
+def __create_bitmap(bits_columns_segments: list[list[int]]) -> list[str]:
+    output_buffer: list[str] = []
+
+    for column_segment in bits_columns_segments:
+        segment_byte = 0
+
+        for bit_index, bit in enumerate(column_segment):
+            segment_byte |= (bit << bit_index)
+
+        output_buffer.append(f'{segment_byte:#04x}')
+
+    return output_buffer
+
+
+def __dump_to_file(img_width: int, img_height: int, output_buffer: list[str], output_path: str):
+    with open(output_path, "w") as out_file:
+        out_file.write(f"#define bitmap_width {img_width}\n")
+        out_file.write(f"#define bitmap_height {img_height}\n")
+
+        out_file.write("\n")
+
+        out_file.write(
+            f"static unsigned char bitmap_bits[] = {{ {', '.join(output_buffer)} }};")
+
+
 def main(args: argparse.Namespace):
     img_path = args.input
     output_path = args.output
@@ -35,48 +83,18 @@ def main(args: argparse.Namespace):
                          255 else 1 for pixel in bw_img_pixels_data]  # type: ignore
 
     print("creating columns segments...")
-    bits_columns_segments: list[list[int]] = []
-
-    for columns_row_id in range(img_height // BITS_PER_BYTE):
-        column_start_offset = columns_row_id * BITS_PER_BYTE
-
-        for column_index in range(img_width):
-            column_bits: list[int] = []
-
-            column_start_bit_index = column_start_offset + \
-                (column_index * img_width)
-
-            for bit_index in range(BITS_PER_BYTE):
-                target_bit_index = column_start_bit_index + bit_index
-                column_bits.append(normalized_pixels[target_bit_index])
-
-            bits_columns_segments.append(column_bits)
+    bits_columns_segments: list[list[int]] = __create_columns(
+        normalized_pixels, img_width, img_height)
 
     print(f"got {len(bits_columns_segments)} total columns")
 
     print("creating bitmap...")
-    output_buffer: list[str] = []
-
-    for column_segment in bits_columns_segments:
-        segment_byte = 0
-
-        for bit_index, bit in enumerate(column_segment):
-            segment_byte |= (bit << bit_index)
-
-        output_buffer.append(f'{segment_byte:#04x}')
+    output_buffer: list[str] = __create_bitmap(bits_columns_segments)
 
     print(f"dumped {len(output_buffer)} bytes")
     print(f"saving to {output_path} ...")
 
-    with open(output_path, "w") as out_file:
-        out_file.write(f"#define bitmap_width {img_width}\n")
-        out_file.write(f"#define bitmap_height {img_height}\n")
-
-        out_file.write("\n")
-
-        out_file.write(
-            f"static unsigned char bitmap_bits[] = {{ {', '.join(output_buffer)} }};")
-
+    __dump_to_file(img_width, img_height, output_buffer, output_path)
     print("done.")
 
 
