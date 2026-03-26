1a0aaa321da48c701bb6905f17ab6b00fd555456
zNitche <53448802+zNitche@users.noreply.github.com>
2026-03-26 20:58:58 +0000

    refactor: renderer - frame to segments conversion

--- lib/include/pico_ssd1306/renderer.h
+++ lib/include/pico_ssd1306/renderer.h
@@ -3,8 +3,9 @@
 #include "pico_ssd1306/defines.h"
 #include "pico_ssd1306/types.h"
 
-void __flatten_frame(uint8_t frame[PICO_SSD1306_HEIGHT][PICO_SSD1306_WIDTH],
-                     uint8_t* output, uint16_t output_buff_length);
+void __load_bitarray_from_flat_bitmap(uint8_t bitmap[], uint8_t width,
+                                      uint8_t height,
+                                      uint8_t output[height][width]);
 void __convert_frame_to_columns_segments(
     uint8_t frame[PICO_SSD1306_HEIGHT][PICO_SSD1306_WIDTH], uint8_t* output,
     uint16_t output_buff_length);
--- lib/src/renderer.c
+++ lib/src/renderer.c
@@ -1,18 +1,24 @@
 #include "pico_ssd1306/renderer.h"
 #include "pico_ssd1306/defines.h"
+#include "pico_ssd1306/helpers.h"
 #include <string.h>
 
-void __flatten_frame(uint8_t frame[PICO_SSD1306_HEIGHT][PICO_SSD1306_WIDTH],
-                     uint8_t* output, uint16_t output_buff_length) {
+void __load_bitarray_from_flat_bitmap(uint8_t bitmap[], uint8_t width,
+                                      uint8_t height,
+                                      uint8_t output[height][width]) {
+    int row_ind = 0;
+    const uint8_t bitmap_length = width * height;
 
-    memset(output, 0x00, output_buff_length);
+    for (int byte_ind = 0; byte_ind < bitmap_length; byte_ind++) {
+        uint8_t bits[8] = {0};
+        __byte_to_bits(bitmap[byte_ind], bits);
 
-    for (int row_ind = 0; row_ind < PICO_SSD1306_HEIGHT; row_ind++) {
-        const int ind_offset = row_ind * PICO_SSD1306_WIDTH;
+        for (int i = 0; i < 8; i++) {
+            output[row_ind][i + (byte_ind * 8)] = bits[i];
+        }
 
-        for (int column_ind = 0; column_ind < PICO_SSD1306_WIDTH;
-             column_ind++) {
-            output[ind_offset + column_ind] = frame[row_ind][column_ind];
+        if (byte_ind % width == 0) {
+            row_ind++;
         }
     }
 }
@@ -22,28 +28,18 @@ void __convert_frame_to_columns_segments(
     uint16_t output_buff_length) {
 
     const uint16_t flattened_buff_length =
-        PICO_SSD1306_HEIGHT * PICO_SSD1306_WIDTH;
-
-    uint8_t flattened_buff[flattened_buff_length];
-    __flatten_frame(frame, flattened_buff, flattened_buff_length);
-
-    memset(output, 0x00, output_buff_length);
+        (PICO_SSD1306_HEIGHT * PICO_SSD1306_WIDTH) / 8;
 
     int output_item_ind = 0;
 
-    for (int col_ind = 0; col_ind < PICO_SSD1306_NUM_PAGES; col_ind++) {
-        const uint8_t column_start_index =
-            col_ind * PICO_SSD1306_WIDTH * PICO_SSD1306_PAGE_HEIGHT;
+    for (int row_page_ind = 0; row_page_ind < PICO_SSD1306_NUM_PAGES; row_page_ind++) {
+        const int row_start_ind = row_page_ind * 8; 
 
-        for (int row_ind = 0; row_ind < PICO_SSD1306_WIDTH; row_ind++) {
+        for (int col_ind = 0; col_ind < PICO_SSD1306_WIDTH; col_ind++) {
             uint8_t byte = 0x00;
 
-            for (int bit_ind = 0; bit_ind < PICO_SSD1306_PAGE_HEIGHT;
-                 bit_ind++) {
-
-                byte |= flattened_buff[bit_ind * PICO_SSD1306_WIDTH +
-                                       column_start_index + row_ind]
-                        << bit_ind;
+            for (int bit_ind = 0; bit_ind < PICO_SSD1306_PAGE_HEIGHT; bit_ind++) {
+                byte |= frame[row_start_ind + bit_ind][col_ind] << bit_ind;
             }
 
             output[output_item_ind] = byte;
