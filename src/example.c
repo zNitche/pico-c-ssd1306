86bf6f9bd4fea03edc112d1763b5261895d75800
zNitche <53448802+zNitche@users.noreply.github.com>
2026-04-10 20:45:20 +0000

    feat: example.c - prints improvements

--- src/example.c
+++ src/example.c
@@ -33,12 +33,12 @@ int main() {
     SSD1306_Frame frame;
     ssd1306_prepare_frame(&frame);
 
-    printf("frame prepared\n");
+    printf("frame prepared, size: %d bytes\n", sizeof(frame));
 
     SSD1306_Bitmap test_bitmap = {.width = 32, .height = 32, .data = NULL};
     ssd1306_load_bitmap(ssd1306_test_bitmap, &test_bitmap);
 
-    printf("loaded test bitmap\n");
+    printf("loaded test bitmap, size: %d bytes\n", sizeof(test_bitmap));
 
     ssd1306_insert_bitmap(&frame, 12, 3, &test_bitmap);
     ssd1306_insert_bitmap(&frame, 68, 12, &test_bitmap);
